fix calculation of state_time in perc_burst_time() (time being active within labeling window) add violin plots of fraction of labeled counts for categories ON (certain percentage active burst time) and OFF

simulator/Experiment.py
@@ -107,7 +107,7 @@ class Experiment:
 
         active_states["begin_time"] = np.maximum(active_states["begin_time"], start)
         active_states["end_time"] = np.minimum(active_states["end_time"], end)
-        active_states.loc["state_time"] = active_states["end_time"] - active_states["begin_time"]
+        active_states["state_time"] = active_states["end_time"] - active_states["begin_time"]
 
         if len(active_states) > 0:
             sum_state_time = active_states["state_time"].sum()

simulator/main.py
@@ -10,28 +10,39 @@ freeze = 550  # freeze 30 minutes after end of last window
 
 params = TranscriptParams(l_01=0.02, l_10=0.02, k_syn=0.16, nr_refractions=1, k_d=0.01)
 
-nr_cells = 100
+nr_cells = 1000
 nr_alleles = 1
 exp = Experiment(nr_cells, nr_alleles, params, windows, freeze)
 
 df_counts = exp.run()
-df_counts["state_80"] = np.where((df_counts.label != "") & (df_counts.perc_label_on > 0.8), "ON", "OFF")
+
+df_counts["fraction"] = df_counts["real_count"] / (df_counts["real_count"] + df_counts["real_count_unlabeled"])
 
 df_counts_eu = df_counts[df_counts.label == "EU"]
-sns.boxplot(data=df_counts, y="real_count", x="state_80")
-sns.swarmplot(data=df_counts, y="real_count", x="state_80", color=".25")
-plt.title("EU transcript real counts with OFF < 80% burst or ON > 80% burst")
-plt.show()
 
+
+def violin_plot_fraction(fraction, perc):
+
+    measure = "state_{perc}".format(perc=perc)
+    df_counts_eu[measure] = np.where(df_counts_eu.perc_label_on > fraction, "ON", "OFF")
+
+    sns.violinplot(data=df_counts_eu, y="fraction", x=measure, inner="stick")
+    plt.ylim(0, None)
+    plt.title("Labeled EU transcript fraction with OFF <= {perc}% burst or ON > {perc}% burst during window".
+              format(perc=perc))
+    plt.show()
+
+
+violin_plot_fraction(0.5, "50")
 
 df_all_arrivals = exp.df_all_arrivals
 
 
 # NB: not all zeroes are shown! (when burst was absent; however, can be derived from unlabeled counts)
-plt.title("Distribution of real labeled mRNA counts over {nr} single cells".format(nr=nr_cells))
-plt.xlabel("count labeled transcripts")
-plt.ylabel("occurence")
-plt.hist(df_counts["real_count"], bins=df_counts["real_count"].max())
-plt.show()
+# plt.title("Distribution of real labeled mRNA counts over {nr} single cells".format(nr=nr_cells))
+# plt.xlabel("count labeled transcripts")
+# plt.ylabel("occurence")
+# plt.hist(df_counts["real_count"], bins=df_counts["real_count"].max())
+# plt.show()
 
 print("Number of counts: {counts}.".format(counts=len(df_counts)))