6537246ee29136434192cd82b1fc4dc99906f95e
Richard Gremmen <gremmen@resharp.nl>
2020-11-03 18:05:14 +0100

    adding real and sampled label counts to plot for different window sizes

--- infer_parameters_example.py
+++ infer_parameters_example.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,12 +9,22 @@ import seaborn as sns
 from simulator.Experiment import Experiment
 from simulator.Transcription import *
 
+if os.name == 'nt':
+    dir_sep = "\\"
+    out_dir = r"D:\26 Battich Oudenaarden transcriptional bursts\runs"
+else:
+    dir_sep = "/"
+    out_dir = "sc_runs"
+plot_dir = out_dir + dir_sep + "infer_parameters_example.plots"
+os.makedirs(plot_dir, exist_ok=True)
+df_filename = "counts_infer_parameters_example.csv"
+
 k_on = 0.005
 
 k_offs = [k * 0.005 for k in range(1, 6)]
 k_d = 0.002
-k_syn = 0.032
-k_eff = 1
+k_syn = 1.6
+k_eff = 0.1
 
 window_lengths = [r*15 for r in range(1, 24)]
 
@@ -95,13 +107,16 @@ def get_windows_and_fix_time(length_window=60, gap=0):
 # show_production_of_mrna()
 
 
-def run_active_state_simulations():
+def run_active_state_simulations(nr_runs):
 
-    nr_runs = 1000
     k_off = 0.02
-    nr_active = []
+    l_counts = []
+
     for w in window_lengths:
         nr_runs_active = 0
+        nr_real_label = 0
+        nr_signal_label = 0
+
         windows, fix_time = get_windows_and_fix_time(length_window=w, gap=0)
 
         params = TranscriptParams(k_on=k_on, k_off=k_off, nr_refractions=2,
@@ -114,24 +129,55 @@ def run_active_state_simulations():
 
         # set complete_trace=True to retrieve the complete trace of transcripts counts (for plotting)
         for run in range(0, nr_runs):
+
             df_dtmc, dtmc_list = trans.run_bursts(fix_time, windows, new_dtmc_trace=True, complete_trace=True)
 
-            # example of calculating percentage active
             label = "4SU"
+            df_transcripts = trans.df_transcripts
+
+            df_labeled_transcripts = df_transcripts[df_transcripts.label == label]
+            if len(df_labeled_transcripts) > 0:
+                nr_real_label = nr_real_label + 1
+
+            len_sample = int(k_eff * len(df_labeled_transcripts))
+            df_sampled = df_transcripts.sample(len_sample, replace=False)
+            if len(df_sampled) > 0:
+                nr_signal_label = nr_signal_label + 1
+
+            # example of calculating percentage active
             perc = Experiment.perc_active_state(windows, df_dtmc, label)
             # print("Percentage active state: {perc}".format(perc=perc))
             if perc > 0:
                 nr_runs_active = nr_runs_active + 1
         print("{label} window contains {nr_runs_active} runs with active state(s) for k_off {k_off} and window {window}".
               format(label=label, k_off=k_off, window=w, nr_runs_active=nr_runs_active))
-        nr_active.append(nr_runs_active)
+        l_counts.append([w, nr_runs_active, nr_real_label, nr_signal_label])
 
-    plt.plot(window_lengths, nr_active)
+    df_counts = pd.DataFrame(l_counts, columns=['window', 'active', 'real', 'signal'])
+    df_counts.to_csv(out_dir + dir_sep + df_filename, sep=';', index=False)
+
+    return df_counts
+
+
+def show_plot(df_counts):
+
+    plt.plot(df_counts.window, df_counts.active)
+    plt.plot(df_counts.window, df_counts.real)
+    plt.plot(df_counts.window, df_counts.signal)
     plt.xlim(0, max(window_lengths) + 15)
     plt.ylim(0, nr_runs)
     plt.xlabel("window size (minutes)")
     plt.ylabel("nr of runs with active state")
-    plt.show()
+    plt.savefig(plot_dir + dir_sep + "counts.svg")
+    # plt.show()
+    plt.close(1)
 
 
-run_active_state_simulations()
+run_sim = False
+nr_runs = 1000
+if run_sim:
+    df_counts = run_active_state_simulations(nr_runs)
+else:
+    df_counts = pd.read_csv(out_dir + dir_sep + df_filename, sep=';')
+
+show_plot(df_counts)
