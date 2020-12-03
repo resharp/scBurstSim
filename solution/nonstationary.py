6e1a419080be157ba8a9e6c232804de1b0185cb6
Richard Gremmen <gremmen@resharp.nl>
2020-12-03 14:53:11 +0100

    starting implementing time dependent solution for non stationary distribution based on Dattani 2016: it still goes wrong for low t, low n (=> negative probabilities) and for certain special parameter conditions (=> divide by zero).

---
+++ solution/nonstationary.py
@@ -0,0 +1,216 @@
+# non stationary solution for P(n, t) taken from Dattani et al, J R Soc Interface 2016
+# https://doi.org/10.1098/rsif.2016.0833
+
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.special.poch.html
+# from scipy.special import gamma, hyp1f1, poch
+import scipy.special as sc
+import numpy as np
+import math
+from utils.utils import round_sig
+import matplotlib.pyplot as plt
+import logging
+import sys
+import os
+
+fac = np.math.factorial  # in Python even a function is an object
+
+out_dir = r"D:\26 Battich Oudenaarden transcriptional bursts\runs\non_stationary.plots"
+
+if os.name == 'nt':
+    dir_sep = "\\"
+else:
+    dir_sep = "/"
+plot_dir = out_dir + dir_sep + "non_stationary.plots"
+os.makedirs(plot_dir, exist_ok=True)
+logging.basicConfig(filename=out_dir + dir_sep + 'nonstationary.log', filemode='w',
+                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+
+
+# t is times in minutes
+def p_non_stationary(n, t, k_on, k_off, k_syn, k_d):
+
+    k_on = k_on / k_d
+    k_off = k_off / k_d
+    mu = k_syn / k_d    # change to symbol mu for better comparison with paper
+
+    factor_a = mu**n / fac(n)
+    # logging.info("factor_a: {}".format(factor_a))
+    sum_a = 0
+
+    for r in range(0, n+1):
+
+        part_a1 = math.comb(n, r)
+        logging.debug("# r={r} ".format(r=r))
+        logging.debug("math.comb(n, r)             : {}".format(part_a1))
+        part_a2 = (-1)**r * sc.poch(-k_on, r) * sc.poch(k_on, n-r) * np.exp(-r*t)
+        logging.debug(part_a2)
+        # there is a problem with this part if k_on + k_off = 1 and r is non-zero
+        part_a3 = 1/sc.poch(1 - k_on - k_off, r)
+        logging.debug("1/sc.poch(1 - k_on - k_off, r): {}".format(part_a3))
+        part_a4 = 1/sc.poch(k_on + k_off, n-r)
+        logging.debug("1/sc.poch(k_on + k_off, n-r): {}".format(part_a4))
+
+        part_a5 = sc.hyp1f1(-k_on + r, 1 - k_on - k_off + r, mu * np.exp(-t))
+        logging.debug("sc.hyp1f1(-k_on + r, 1 - k_on - k_off + r, k_syn*np.exp(-t): {}".format(part_a5))
+        part_a6 = sc.hyp1f1(k_on + n - r, k_on + k_off + n - r, -mu)
+        logging.debug(part_a6)
+
+        part_a = part_a1 * part_a2 * part_a3 * part_a4 * part_a5 * part_a6
+        sum_a = sum_a + part_a
+
+    sum_a = sum_a * factor_a
+
+    fac_b1 = k_on * mu**(n + 1) * np.exp(-(k_on + k_off) * t)
+    fac_b2 = 1 / (
+            (k_on + k_off) *
+            (1 - k_on - k_off) *
+            fac(n)
+            )
+    factor_b = fac_b1 * fac_b2
+    sum_b = 0
+    for r in range(0, n+1):
+
+        part_b = part_of_sum(k_on, k_off, mu, n, r, t)
+
+        sum_b = sum_b + part_b
+
+    sum_b = factor_b * sum_b
+
+    fac_c1 = k_on * mu**n * np.exp(-(k_on + k_off) * t)
+    fac_c2 = fac_b2
+
+    factor_c = fac_c1 * fac_c2
+    sum_c = 0
+    for r in range(0, n):
+
+        # the sum part for C seems to be identical to the sum part of B
+        # with n replaced by n-1
+        # except for the n! in the denominator
+        # is it a typo? or is it to be expected?
+        part_c = part_of_sum(k_on, k_off, mu, n-1, r, t)
+        sum_c = sum_c + part_c
+
+    sum_c = factor_c * sum_c
+
+    total_sum = sum_a + sum_b + sum_c
+
+    logging.debug("sum_a for n={n}: {sum_a}".format(n=n, sum_a=sum_a))
+    logging.debug("sum_b for n={n}: {sum_b}".format(n=n, sum_b=sum_b))
+    logging.debug("sum_c for n={n}: {sum_c}".format(n=n, sum_c=sum_c))
+    logging.info("total_sum for n={n}: {total_sum}".format(n=n, total_sum=total_sum))
+
+    return total_sum
+
+
+def part_of_sum(k_on, k_off, mu, n, r, t):
+
+    part_b1 = math.comb(n, r)
+    part_b2 = (-1) ** r * sc.poch(-k_off, r) * sc.poch(1 - k_off, n - r) * np.exp(-r * t)
+    part_b3 = 1 / sc.poch(1 + k_on + k_off, r)
+    part_b4 = 1 / sc.poch(2 - k_on - k_off, n - r)
+    part_b5 = sc.hyp1f1(k_off + r,
+                        1 + k_on + k_off + r,
+                        mu * np.exp(-t))
+    # NB: some terms in the Pochhammer's reappear in 1F1
+    # and may cancel each other and this may avoid dividing by zero?
+    part_b6 = sc.hyp1f1(1 - k_off + n - r,
+                        2 - k_on - k_off + n - r,
+                        -mu)
+    part_b = part_b1 * part_b2 * part_b3 * part_b4 * part_b5 * part_b6
+
+    return part_b
+
+
+def p_stationary(n, k_on, k_off, k_syn, k_d):
+
+    k_on = k_on / k_d
+    k_off = k_off / k_d
+    k_syn = k_syn / k_d
+
+    part1 = k_syn**n/fac(n)
+    part2 = sc.poch(k_on, n)/sc.poch(k_on + k_off, n)
+    part3 = sc.hyp1f1(k_on + n, k_on + k_off + n, - k_syn)
+
+    ret_val = part1 * part2 * part3
+
+    return ret_val
+
+
+def create_distribution(k_on, k_off, k_syn, k_d):
+    y_list = []
+    x_list = []
+    x = 0
+    y = 1
+    while y > 1e-6:
+        y = p_stationary(x, k_on, k_off, k_syn, k_d)
+
+        x_list.append(x)
+        y_list.append(y)
+        x = x + 1
+    return x_list, y_list
+
+
+def plot_distribution(k_on, k_off, k_syn, k_d):
+
+    x_list, y_list = create_distribution(k_on, k_off, k_syn, k_d)
+
+    plt.step(x_list, y_list, where="post")
+    plt.show()
+    plt.close(1)
+
+
+k_on = 0.021
+k_off = 0.021
+k_syn = 0.2
+k_d = 0.02
+
+# plot_distribution("test", k_on, k_off, k_syn, k_d)
+
+
+def create_non_stationary_distribution(time, k_on, k_off, k_syn, k_d):
+
+    x_list = []
+    y_list = []
+
+    # e.g. for n in range(0, 120):
+    total_p = 0
+    n = 0
+    p_ns = 1  # initialize at large value
+    while p_ns > 1e-6:
+        p_ns = p_non_stationary(n, time, k_on, k_off, k_syn, k_d)
+        x_list.append(n)
+        y_list.append(p_ns)
+        total_p = total_p + p_ns
+        n = n + 1
+
+    logging.info("total_p : {}".format(total_p))
+
+    return x_list, y_list
+
+
+times = [2.3, 2.5] + list(range(3, 6, 1))
+# times = list(range(3, 7))
+# times = []
+# times = [1]
+
+for time in times:
+
+    x_list, y_list = create_non_stationary_distribution(time, k_on, k_off, k_syn, k_d)
+
+    plt.step(x_list, y_list, where="post", label="t={time}".format(time=time))
+
+x_list_stat, y_list_stat = create_distribution(k_on, k_off, k_syn, k_d)
+
+plt.step(x_list_stat, y_list_stat, where="post", label="stationary")
+
+plt.legend(title='Time:')
+plt.show()
+plt.close(1)
+# logging.debug(sc.gamma(0))
+
+# logging.debug(sc.poch(1 - k_on - k_off, 0))
+# logging.debug(sc.poch(1 - k_on - k_off, 1))
+# logging.debug(sc.poch(1 - k_on - k_off, 2))
+
