fad2b40aa1ff51e3a25dab3b7e8803526f89784c
Richard Gremmen <gremmen@resharp.nl>
2021-01-07 09:57:14 +0100

    explore: add fit of active state fraction to distribution to beta distribution model

--- analyze_active_state.py
+++ analyze_active_state.py
@@ -3,7 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from simulator.StrategyReader import StrategyReader
 import numpy as np
-import seaborn as sns
+from scipy.optimize import curve_fit
+from scipy.stats import beta
 
 if os.name == 'nt':
     dir_sep = "\\"
@@ -39,26 +40,31 @@ def get_parameters(strategy):
     return df_strategy.k_on.item(), df_strategy.k_off.item()
 
 
-def plot_distribution_active_state():
+def create_distribution(df_counts, strategy):
+    df_counts_label = df_counts[(df_counts.label == label_2) & (df_counts.strategy == strategy)]
 
-    strategies = ["first_example", "second_example", "third_example", "bimodal", "powerlaw"]
-    # strategies = ["powerlaw"]
+    # convert to density for perc (=percentage active state)
+
+    bins = np.linspace(0, 1, 100)
+
+    pdf = np.histogram(df_counts_label.perc, bins=bins)[0]
+
+    bins = np.delete(bins, 0)  # remove first
+    sum_pdf = sum(pdf)
+    norm_pdf = (pdf / sum_pdf) * len(bins)
+
+    return bins, norm_pdf
+
+
+def plot_distribution_active_state(strategies):
 
     for strategy in strategies:
 
-        df_counts_label = df_counts[(df_counts.label == label_2) & (df_counts.strategy == strategy)]
+        bins, norm_pdf = create_distribution(df_counts, strategy)
 
-        # convert to density for perc (=percentage active state)
+        # plt.step(bins, norm_pdf, where="post")
+        plt.plot(bins, norm_pdf)
 
-        bins = np.arange(0, 1.01, 0.01)
-
-        pdf = np.histogram(df_counts_label.perc, bins=bins)[0]
-
-        bins = np.delete(bins, 0)  # remove first
-        sum_pdf = sum(pdf)
-        norm_pdf = pdf/sum_pdf
-
-        plt.step(bins, norm_pdf, where="post")
         plt.ylim(0, None)
         plt.xlabel("percentage active state")
         plt.ylabel("chance")
@@ -73,7 +79,43 @@ def plot_distribution_active_state():
         plt.close(1)
 
 
-# create distributions for percentage of active state ("Hidden Markov distribution")
-plot_distribution_active_state()
+def beta_model(x, a, b):
+    return beta.pdf(x, a, b)
+
+
+# fit to beta distribution
+# TODO: find a way to avoid local optima, the result is very dependent on "expected" (initial conditions of
+# fitting procedure, e.g. for t=180 and strategy="third_example" you only find a good fit if you expect
+# both a > 1 and b > 1; the peak at perc=0 and perc=1 are not fitted)
+def fit_distribution_active_state_to_beta_distribution(strategy):
+
+    bins, norm_pdf = create_distribution(df_counts, strategy)
+
+    expected = (1.01, 1.01)
+
+    popt, pcov = curve_fit(beta.pdf, bins, norm_pdf, expected)
+
+    a = popt[0]
+    b = popt[1]
+
+    # a = 0.5
+    # b = 0.5
+
+    # x = np.linspace(beta.ppf(0.01, a, b), beta.ppf(0.99, a, b), 100)
+    x = np.linspace(0, 1, 100)
+    plt.plot(x, beta.pdf(x, a, b), 'r-', lw=2, alpha=0.6, label='beta pdf')
+    plt.plot(bins, norm_pdf, 'b-', lw=2, alpha=0.6, label='norm pdf')
+    plt.title("estimated a: {a}, b: {b}".format(a=a, b=b))
+    plt.show()
+    plt.close(1)
+
+
+# build intuition: create distributions for percentage of active state ("active state fraction distribution")
+strategies = ["first_example", "second_example", "third_example", "bimodal", "powerlaw"]
+plot_distribution_active_state(strategies)
+
+# explore: try to fit to beta distribution
+strategy = "third_example"
+fit_distribution_active_state_to_beta_distribution(strategy)
 
 
