1c9896a5bf0825f499e2230b777a425ce7813f19
Richard Gremmen <gremmen@resharp.nl>
2021-01-07 09:56:13 +0100

    improve code and comments

--- solution/nonstat_markov.py
+++ solution/nonstat_markov.py
@@ -8,7 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def P_00(k_on, k_off, t):
+def p_00(k_on, k_off, t):
     la = k_on
     mu = k_off
 
@@ -17,7 +17,7 @@ def P_00(k_on, k_off, t):
     return ret_val
 
 
-def P_10(k_on, k_off, t):
+def p_10(k_on, k_off, t):
     la = k_on
     mu = k_off
 
@@ -25,34 +25,47 @@ def P_10(k_on, k_off, t):
 
     return ret_val
 
+
+def create_transition_chances(k_on, k_off):
+
+    wins = [j*15 for j in range(0, 9)]
+
+    p_00s = []
+    p_10s = []
+
+    for t in wins:
+
+        p_00s.append(p_00(k_on, k_off, t))
+        p_10s.append(p_10(k_on, k_off, t))
+
+    p_01s = [1 - p for p in p_00s]
+    p_11s = [1 - p for p in p_10s]
+
+    return wins, (p_00s, p_10s, p_01s, p_11s)
+
+
+def plot_transition_chances(k_on, k_off, wins, p_00s, p_10s, p_01s, p_11s):
+    plt.plot(wins, p_00s, 'o-', label="P_00")
+    plt.plot(wins, p_10s, 'o-', label="P_10")
+    # plt.plot(wins, p_01s, 'o-', label="P_01")
+    # plt.plot(wins, p_11s, 'o-', label="P_11")
+
+    title = "k_on={k_on};k_off={k_off}. Chances of being in state x_t given state x_0," \
+            " with e.g. P_00 = P(x_t=0 | x_0=0)".\
+        format(k_on=k_on, k_off=k_off)
+    plt.title(title)
+
+    plt.ylim(0, 1)
+    plt.legend()
+    plt.xlabel("time in minutes")
+    plt.ylabel("chance")
+    plt.show()
+    plt.close(1)
+
+
 k_on = 0.01
 k_off = 0.04
 
-wins = [j*15 for j in range(0, 9)]
+wins, (p_00s, p_10s, p_01s, p_11s) = create_transition_chances(k_on=k_on, k_off=k_off)
 
-p_00s = []
-p_10s = []
-
-for t in wins:
-
-    p_00s.append(P_00(k_on, k_off, t))
-    p_10s.append(P_10(k_on, k_off, t))
-
-p_01s = [1 - p for p in p_00s]
-p_11s = [1 - p for p in p_10s]
-
-
-plt.plot(wins, p_00s, 'o-', label="P_00")
-plt.plot(wins, p_10s, 'o-', label="P_10")
-# plt.plot(wins, p_01s, 'o-', label="P_01")
-# plt.plot(wins, p_11s, 'o-', label="P_11")
-title = "k_on={k_on};k_off={k_off}. Chances of being in state x_t given state x_0, with e.g. P_00 = P(x_t=0 | x_0=0)".\
-    format(k_on=k_on, k_off=k_off)
-plt.title(title)
-plt.ylim(0, 1)
-plt.legend()
-plt.xlabel("time in minutes")
-plt.ylabel("chance")
-plt.show()
-plt.close(1)
-
+plot_transition_chances(k_on, k_off, wins, p_00s, p_10s, p_01s, p_11s)
