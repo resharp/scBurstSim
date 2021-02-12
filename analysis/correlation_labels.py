a3d8f718fff1980516d73f0c56eecf24878a63a8
Richard Gremmen <gremmen@resharp.nl>
2021-02-12 10:30:59 +0100

    run correlation against length of windows for genes with a fixed period of 2h; download all runs with 500 cells from server

--- analysis/correlation_labels.py
+++ analysis/correlation_labels.py
@@ -15,8 +15,8 @@ WINDOW_START = 0; WINDOW_END = 1; WINDOW_LABEL = 2
 if os.name == 'nt':
     dir_sep = "\\"
     # TODO: set your own out directory
-    out_dir = r"D:\26 Battich Oudenaarden transcriptional bursts\runs"
-    # out_dir = r"D:\26 Battich Oudenaarden transcriptional bursts\runs_on_server"
+    # out_dir = r"D:\26 Battich Oudenaarden transcriptional bursts\runs"
+    out_dir = r"D:\26 Battich Oudenaarden transcriptional bursts\runs_on_server"
 else:
     dir_sep = "/"
     out_dir = "sc_runs"
@@ -26,7 +26,7 @@ efficiency = 100
 gap = 0
 label_1 = "EU"
 label_2 = "4SU"
-len_win = 60
+len_win = 120
 
 plot_dir = out_dir + dir_sep + "correlation_labels.plots"
 os.makedirs(plot_dir, exist_ok=True)
@@ -46,7 +46,8 @@ df_strategies = sr.df_strategies
 
 
 def scatter_plot_for_allele(df_counts, strategy):
-    df_counts_allele = df_counts[df_counts_12.strategy == strategy]
+
+    df_counts_allele = df_counts[df_counts.strategy == strategy]
 
     corr, _ = pearsonr(df_counts_allele.norm_count_1, df_counts_allele.norm_count_2)
 
@@ -119,21 +120,98 @@ def make_box_plot_for_periods(df, measure, agg_field, tran_type):
     plt.close(1)
 
 
-df_counts = normalize_counts(df_counts)
-df_counts_12 = merge_label_counts(df_counts, label_1, label_2)
+def make_box_plot_for_len_win(df, measure, agg_field, tran_type):
+
+    sns.set_theme(style="whitegrid")
+
+    pattern = "_" + tran_type
+    df_type = df[df.strategy.str.contains(pattern)]
+
+    df_type["len_win_str"] = df_type.len_win.map(str)
+
+    plt.figure(figsize=(12, 5))
+    plt.title("Pearson correlation for different window lengths, type={}".format(tran_type))
+    sns.set(style="ticks")
+
+    b = sns.boxplot(x=agg_field, y=measure, data=df_type,
+                    palette="vlag", orient="v")
+
+    sns.set(font_scale=0.8)
+
+    sns.swarmplot(x=agg_field, y=measure, data=df_type,
+                  size=2, color=".3", linewidth=0, orient="v")
+
+    plt.xlabel("Length window (minutes)")
+    plt.ylabel("Pearson correlation between normalized counts two labels")
+
+    fig_name = plot_dir + dir_sep + "boxplot_correlation_{agg_field}_{type}_{len_win}.svg".format(
+        type=tran_type, agg_field=agg_field, len_win=len_win)
+    plt.savefig(fig_name)
+    plt.close(1)
 
 
-df_corr = calculate_corr_and_save(df_counts_12)
-df_corr = add_coord_group_to_strategy(df_corr)
+def run_and_plot_one_correlation(df_counts):
 
-strategy = df_corr.head(1).strategy.item()
-scatter_plot_for_allele(df_counts_12, strategy)
+    df_counts = normalize_counts(df_counts)
+    df_counts_12 = merge_label_counts(df_counts, label_1, label_2)
 
-strategy = df_corr.tail(1).strategy.item()
-scatter_plot_for_allele(df_counts_12, strategy)
+    df_corr = calculate_corr_and_save(df_counts_12)
+    df_corr = add_coord_group_to_strategy(df_corr)
 
-df_corr["period"] = df_corr.strategy.str.split("_", expand=True)[4]
+    strategy = df_corr.head(1).strategy.item()
+    scatter_plot_for_allele(df_counts_12, strategy)
 
-for tran_type in ["S", "F"]:
-    make_box_plot_for_periods(df_corr, measure="corr", agg_field="period", tran_type=tran_type)
+    strategy = df_corr.tail(1).strategy.item()
+    scatter_plot_for_allele(df_counts_12, strategy)
+
+    df_corr["period"] = df_corr.strategy.str.split("_", expand=True)[4]
+
+    for tran_type in ["S", "F"]:
+        make_box_plot_for_periods(df_corr, measure="corr", agg_field="period", tran_type=tran_type)
+
+
+# combine all correlation files into one and add an extra column for window size
+def run_all_correlations():
+    corr_list = []
+    window_lengths = [15, 30, 45, 60, 75, 90, 105, 120]
+    for len_win in window_lengths:
+
+        filename_counts = out_dir + dir_sep + "df_counts_W{len_win}_G{gap}.csv".format(
+            len_win=len_win, gap=gap, eff=efficiency)
+
+        # df_counts are counts of two labeling windows (single window length)
+        df_counts = pd.read_csv(filename_counts, sep=';')
+
+        df_counts = normalize_counts(df_counts)
+        df_counts_12 = merge_label_counts(df_counts, label_1, label_2)
+
+        df_counts_12["len_win"] = len_win
+
+        df_corr = calculate_corr_and_save(df_counts_12)
+        df_corr["len_win"] = len_win
+
+        corr_list.append(df_corr)
+
+    df_corr_all = pd.concat(corr_list)
+    df_corr_all["period"] = df_corr_all.strategy.str.split("_", expand=True)[4]
+
+    return df_corr_all
+
+
+# run_and_plot_one_correlation(df_counts)
+
+corr_name = "{od}{dir_sep}df_corr_all_G{gap}.csv".format(
+    od=plot_dir, dir_sep=dir_sep, gap=gap)
+
+run_corr = False
+if run_corr:
+    df_corr_all = run_all_correlations()
+    df_corr_all.to_csv(path_or_buf=corr_name, sep=';', index=False)
+else:
+    df_corr_all = pd.read_csv(corr_name, sep=';')
+
+period = 2
+df_one_period = df_corr_all[df_corr_all.period == period]
+
+make_box_plot_for_len_win(df_one_period, "corr", "len_win_str", "F")
 
---
+++ scripts/download_runs.sh
@@ -0,0 +1,4 @@
+
+
+# download runs for windows in range 15 - 120 to current directory
+scp richard@alive.bio.uu.nl:/hosts/linuxhome/mutant1/tmp/richard/sc_runs_*/df_counts_W*_G0.csv .
