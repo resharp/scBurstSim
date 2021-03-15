a1bfae5b97779a63fb3097a025c43b846485520f
Richard Gremmen <gremmen@resharp.nl>
2021-03-15 18:52:29 +0100

    use various initial guesses and use the best fit

--- analysis/correlation_fit_period.py
+++ analysis/correlation_fit_period.py
@@ -31,27 +31,41 @@ def dampening_fluctuation(x, period, amplitude, offset, dampening):
 
 def fit_period(series):
 
-    inital_guesses = [60, 120, 180, 240, 300]
-    min_delta_corr = 0.5
+    min_delta_corr = 0.4
 
     min_corr = min(series["corr"])
     max_corr = max(series["corr"])
 
     if np.abs(min_corr - max_corr) < min_delta_corr:
-        period = -1
+        df = pd.DataFrame([[-1, -1, -1]], columns=["guess", "period", "error_period"])
+        return df
 
-    else:
-        expected = (120, 1, -1, 0.01)
+    results_debug = []
+    inital_guesses = [60, 120, 180, 240, 300]
+    for guess in inital_guesses:
+
+        expected = (guess, 1, -1, 0.01)
 
         try:
             popt, pcov = curve_fit(dampening_fluctuation, series.len_win, series["corr"], p0=expected, maxfev=1000)
             period = popt[0]
+            error_period = pcov[0, 0]
         except RuntimeError:  # this happens mostly when exceeding maxfev
             period = -2
+            error_period = -2
+        results_debug.append((guess, period, error_period))
 
-    # if period != -1:
-    #     print(period)
-    df = pd.DataFrame([[period]], columns=["period"])
+    df_debug = pd.DataFrame(results_debug, columns=["guess", "period", "error_period"])
+
+    df_debug = df_debug[df_debug.error_period > 0].reset_index()
+    df_debug.drop('index', axis=1, inplace=True)
+
+    if len(df_debug > 0):
+        df = df_debug.iloc[[df_debug['error_period'].idxmin()], :]
+        df['period'] = np.abs(df['period'])  # due to cosine symmetry around 0 negative values of period may occur
+    else:
+        df = pd.DataFrame([[-2, -2, -2]], columns=["guess", "period", "error_period"])
+
     return df
 
 
@@ -67,11 +81,9 @@ df_corr_all = pd.read_csv(corr_name, sep=';')
 
 # only take the deterministically fluctuating genes with a period of 2 hours
 df_corr_all = df_corr_all[(df_corr_all.strategy.str.endswith("_2")) & (df_corr_all.strategy.str.contains("F"))]
+# df_corr_all = df_corr_all[(df_corr_all.strategy.str.endswith("_2"))]
 
 df_predictions = df_corr_all.groupby(['strategy']).apply(fit_period).reset_index()
-
-debug = True
-
 
 plot_dir = r"{}{}runs{}{}".format(prj_dir, dir_sep, dir_sep, "correlation_fit_period.plots")
 os.makedirs(plot_dir, exist_ok=True)
